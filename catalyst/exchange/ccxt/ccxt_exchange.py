1da2ad60962a8385257e96611c0388bd8b23cc0b
red-eagle-eye <hugoslb@hotmail.com>
2021-11-30 12:50:14 +0000

    tickers_cache: avoid raising an error for a missing asset when populating the tickers cache

--- catalyst/exchange/ccxt/ccxt_exchange.py
+++ catalyst/exchange/ccxt/ccxt_exchange.py
@@ -1259,7 +1259,7 @@ class CCXT(Exchange):
 
         now = datetime.datetime.utcnow()
         if self.tickers_cache_dt is None or not self._same_minute(now, self.tickers_cache_dt):
-            self.tickers_cache = self.tickers_internal(self.active_assets, on_ticker_error)
+            self.tickers_cache = self.tickers_internal(self.active_assets, "warn")
             self.tickers_cache_dt = now
 
         return dict(filter(lambda elem: elem[0] in assets, self.tickers_cache.items()))
